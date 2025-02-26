ea5c3388c663db63513f4c007a02f4fbab9e935e
bootcat55 <sloanb23517@gmail.com>
2025-02-27 01:39:39 +0200

    cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    devtool: 'source-map', // Enable source maps
     module: {
         rules: [
             {
