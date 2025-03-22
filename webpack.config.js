3d34d3a57ecabdeeccb87095136564f5679f8b00
bootcat55 <sloanb23517@gmail.com>
2025-03-22 02:20:35 +0200

    Test mapbox update

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './src/index.js', // Entry point for the new index.js file
+    entry: './src/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
@@ -25,7 +25,23 @@ module.exports = {
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader'],
             },
+            {
+                test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+                type: 'asset/resource', // Handle static assets
+            },
         ],
+    },
+    resolve: {
+        fallback: {
+            // Add fallbacks for Node.js core modules
+            fs: false,
+            path: require.resolve('path-browserify'),
+            os: require.resolve('os-browserify/browser'),
+            crypto: require.resolve('crypto-browserify'),
+            stream: require.resolve('stream-browserify'),
+            vm: require.resolve('vm-browserify'), // Add this line
+            buffer: require.resolve('buffer/'), // Add this line
+        },
     },
     devServer: {
         static: {
